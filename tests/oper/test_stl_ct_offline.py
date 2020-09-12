525ab51ce46072dcf20549574c9342fe9aacdc98
TomoyaYamaguchi <tomoya.yamaguchi@toyota.com>
2020-09-11 22:55:26 -0400

    add fall oprator

--- tests/oper/test_stl_ct_offline.py
+++ tests/oper/test_stl_ct_offline.py
@@ -2,6 +2,7 @@ import unittest
 from rtamt.operation.stl_ct_offline.not_operation import NotOperation
 from rtamt.operation.stl_ct_offline.abs_operation import AbsOperation
 from rtamt.operation.stl_ct_offline.rise_operation import RiseOperation
+from rtamt.operation.stl_ct_offline.fall_operation import FallOperation
 
 class TestSTLoffline(unittest.TestCase):
 
@@ -37,6 +38,17 @@ class TestSTLoffline(unittest.TestCase):
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 1st example:\nExpected output: %s\nComputed output: %s" % (
                                  out_expected, out_computed))
+
+    def test_fall(self):
+        oper = FallOperation()
+        in_data = [[5, 3], [5.3, -1], [5.75, 2], [6.5, -5], [6.75, 6], [9, 5], [9.25, 4], [10, 2]]
+        out_expected = [[5, 0], [5.3, 4], [5.75, -3], [6.5, 7], [6.75, -11], [9, 1], [9.25, 1], [10, 2]]
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 1st example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
 
 if __name__ == '__main__':
     unittest.main()
