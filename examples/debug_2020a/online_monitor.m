636fc77b93751c04a95e0dbd864bce75c92a5ca7
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2020-10-13 11:03:33 +0200

    Added a debug dir

---
+++ examples/debug_2020a/online_monitor.m
@@ -0,0 +1,196 @@
+function online_monitor(block)
+%ONLINE_MONITOR online monitoring s-function
+
+%%
+%% The setup method is used to set up the basic attributes of the
+%% S-function such as ports, parameters, etc. Do not add any other
+%% calls to the main body of the function.
+%%
+setup(block);
+
+%endfunction
+
+%% Function: setup ===================================================
+%% Abstract:
+%%   Set up the basic characteristics of the S-function block such as:
+%%   - Input ports
+%%   - Output ports
+%%   - Dialog parameters
+%%   - Options
+%%
+%%   Required         : Yes
+%%   C-Mex counterpart: mdlInitializeSizes
+%%
+function setup(block)
+
+% Register number of ports
+block.NumInputPorts  = 1;
+block.NumOutputPorts = 1;
+
+% Setup port properties to be inherited or dynamic
+block.SetPreCompInpPortInfoToDynamic;
+block.SetPreCompOutPortInfoToDynamic;
+
+% Override input port properties
+block.InputPort(1).Dimensions        = -1;
+block.InputPort(1).DatatypeID  = 0;  % double
+block.InputPort(1).Complexity  = 'Real';
+%block.InputPort(1).DirectFeedthrough = true;
+
+% Override output port properties
+block.OutputPort(1).Dimensions       = 1;
+block.OutputPort(1).DatatypeID  = 0; % double
+block.OutputPort(1).Complexity  = 'Real';
+
+% Register parameters
+block.NumDialogPrms     = 0;
+
+% Register sample times
+%  [0 offset]            : Continuous sample time
+%  [positive_num offset] : Discrete sample time
+%
+%  [-1, 0]               : Inherited sample time
+%  [-2, 0]               : Variable sample time
+block.SampleTimes = [-1 0];
+
+% Specify the block simStateCompliance. The allowed values are:
+%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
+%    'DefaultSimState', < Same sim state as a built-in block
+%    'HasNoSimState',   < No sim state
+%    'CustomSimState',  < Has GetSimState and SetSimState methods
+%    'DisallowSimState' < Error out when saving or restoring the model sim state
+block.SimStateCompliance = 'DefaultSimState';
+
+%% -----------------------------------------------------------------
+%% The MATLAB S-function uses an internal registry for all
+%% block methods. You should register all relevant methods
+%% (optional and required) as illustrated below. You may choose
+%% any suitable name for the methods and implement these methods
+%% as local functions within the same file. See comments
+%% provided for each function for more information.
+%% -----------------------------------------------------------------
+
+block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
+block.RegBlockMethod('InitializeConditions', @InitializeConditions);
+block.RegBlockMethod('Start', @Start);
+block.RegBlockMethod('Outputs', @Outputs);     % Required
+block.RegBlockMethod('Update', @Update);
+%block.RegBlockMethod('Derivatives', @Derivatives);
+%block.RegBlockMethod('Terminate', @Terminate); % Required
+
+%end setup
+
+%%
+%% PostPropagationSetup:
+%%   Functionality    : Setup work areas and state variables. Can
+%%                      also register run-time methods here
+%%   Required         : No
+%%   C-Mex counterpart: mdlSetWorkWidths
+%%
+function DoPostPropSetup(block)
+    block.NumDworks = 0;
+
+%%
+%% InitializeConditions:
+%%   Functionality    : Called at the start of simulation and if it is 
+%%                      present in an enabled subsystem configured to reset 
+%%                      states, it will be called when the enabled subsystem
+%%                      restarts execution to reset the states.
+%%   Required         : No
+%%   C-MEX counterpart: mdlInitializeConditions
+%%
+function InitializeConditions(block)
+
+    
+
+%end InitializeConditions
+
+
+%%
+%% Start:
+%%   Functionality    : Called once at start of model execution. If you
+%%                      have states that should be initialized once, this 
+%%                      is the place to do it.
+%%   Required         : No
+%%   C-MEX counterpart: mdlStart
+%%
+function Start(block)
+    import py.rtamt.STLIOSpecification;
+    spec = STLIOSpecification(1);
+    declare_var(spec, 'pc', 'float');
+    declare_var(spec, 'lep', 'float');
+    declare_var(spec, 'out', 'float');
+    set_var_io_type(spec, 'pc', 'input');
+    set_var_io_type(spec, 'lep', 'output');
+    set_var_io_type(spec, 'out', 'output');
+    spec.iosem = 'standard';
+    spec.spec = 'out = (rise(pc >= 0.9)) implies (eventually[0:100] always[0:100] (abs(pc - lep) <= 3))';
+
+    parse(spec);
+    
+    set_param(block.BlockHandle, 'UserData', spec);
+
+%end Start
+
+%%
+%% Outputs:
+%%   Functionality    : Called to generate block outputs in
+%%                      simulation step
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlOutputs
+%%
+function Outputs(block)
+
+    val = block.InputPort(1).Data;
+    
+    spec = get_param(block.BlockHandle, 'UserData');
+     
+    pc_tuple = py.tuple({'pc',val(1)});
+    lep_tuple = py.tuple({'lep',val(2)});
+    l = py.list;
+    append(l, pc_tuple);
+    append(l, lep_tuple);
+    out = update(spec,0, l);
+     
+     set_param(block.BlockHandle, 'UserData', spec);
+     
+     %if(out <= 0)
+     %    set_param(bdroot, 'SimulationCommand', 'stop');
+     %end
+     
+     block.OutputPort(1).Data = out;
+
+%end Outputs
+
+%%
+%% Update:
+%%   Functionality    : Called to update discrete states
+%%                      during simulation step
+%%   Required         : No
+%%   C-MEX counterpart: mdlUpdate
+%%
+function Update(block)
+
+%end Update
+
+%%
+%% Derivatives:
+%%   Functionality    : Called to update derivatives of
+%%                      continuous states during simulation step
+%%   Required         : No
+%%   C-MEX counterpart: mdlDerivatives
+%%
+function Derivatives(block)
+
+%end Derivatives
+
+%%
+%% Terminate:
+%%   Functionality    : Called at the end of simulation for cleanup
+%%   Required         : Yes
+%%   C-MEX counterpart: mdlTerminate
+%%
+function Terminate(block)
+
+%end Terminate
+
