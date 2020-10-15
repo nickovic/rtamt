62af864a54300c46b066b3736d40e998a400e1d8
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2020-10-15 17:53:54 -0400

    add always operator

---
+++ rtamt/operation/stl_ct_offline/always_operation.py
@@ -0,0 +1,29 @@
+from rtamt.operation.abstract_operation import AbstractOperation
+import numpy
+
+class AlwaysOperation(AbstractOperation):
+    def __init__(self):
+        pass
+
+    def update(self, *args, **kargs):
+        pass
+
+    def offline(self, *args, **kargs):
+        out = []
+        input_list = args[0]
+
+        i=0
+        while i < len(input_list):
+            temp_nums = [ i[1] for i in input_list[i:len(input_list)] ]
+            temp_min = min(temp_nums)
+            min_indexs = [i for i, x in enumerate(temp_nums) if x == temp_min]
+            out.append([input_list[i][0], temp_min])
+
+            if i == len(input_list)-1:
+                break
+            elif min_indexs[-1] == 0:
+                i = i + 1
+            else:
+                i = i+min_indexs[-1]
+
+        return out
