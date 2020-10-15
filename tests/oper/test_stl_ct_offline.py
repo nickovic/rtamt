1efd8fe4c8fb3265eba38cdfc59cd9c897ad1ec1
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2020-10-15 19:17:57 -0400

    add historically operator

--- tests/oper/test_stl_ct_offline.py
+++ tests/oper/test_stl_ct_offline.py
@@ -9,6 +9,7 @@ from rtamt.operation.stl_ct_offline.abs_operation import AbsOperation
 from rtamt.operation.stl_ct_offline.rise_operation import RiseOperation
 from rtamt.operation.stl_ct_offline.fall_operation import FallOperation
 from rtamt.operation.stl_ct_offline.always_operation import AlwaysOperation
+from rtamt.operation.stl_ct_offline.historically_operation import HistoricallyOperation
 
 class TestSTLoffline(unittest.TestCase):
 
@@ -95,6 +96,52 @@ class TestSTLoffline(unittest.TestCase):
         oper = AlwaysOperation()
         in_data = [[5, 3], [6, 4], [7, 5]]
         out_expected = [[5, 3], [6, 4], [7, 5]]
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 5th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+    def test_historically(self):
+        oper = HistoricallyOperation()
+        in_data = [[5, 3], [5.3, 1], [5.75, 2], [6.5, 5], [6.75, 6], [9, 5], [9.25, 4], [10, 2]]
+        out_expected = [[5, 6], [9, 5], [9.25, 4], [10, 2]]
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 1st example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = HistoricallyOperation()
+        in_data = [[5, 3]]
+        out_expected = [[5, 3]]
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 2nd example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = HistoricallyOperation()
+        in_data = []
+        out_expected = []
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 3rd example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = HistoricallyOperation()
+        in_data = [[5, 3], [6, 2], [7, 1]]
+        out_expected = [[5, 3], [6, 2], [7, 1]]
+        out_computed = oper.offline(in_data)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 4th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = HistoricallyOperation()
+        in_data = [[5, 3], [6, 4], [7, 5]]
+        out_expected = [[5, 5], [7, 5]]
         out_computed = oper.offline(in_data)
 
         self.assertListEqual(out_expected, out_computed,
