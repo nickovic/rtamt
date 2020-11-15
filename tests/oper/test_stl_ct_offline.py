80b207acb5d2a889f91bf6335f56923e3472446e
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2020-11-14 23:20:13 -0500

    add and operator

--- tests/oper/test_stl_ct_offline.py
+++ tests/oper/test_stl_ct_offline.py
@@ -12,6 +12,7 @@ from rtamt.operation.stl_ct_offline.always_operation import AlwaysOperation
 from rtamt.operation.stl_ct_offline.eventually_operation import EventuallyOperation
 from rtamt.operation.stl_ct_offline.always_bounded_operation import AlwaysBoundedOperation
 from rtamt.operation.stl_ct_offline.eventually_bounded_operation import EventuallyBoundedOperation
+from rtamt.operation.stl_ct_offline.and_operation import AndOperation
 
 class TestSTLoffline(unittest.TestCase):
 
@@ -413,6 +414,78 @@ class TestSTLoffline(unittest.TestCase):
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 8th example:\nExpected output: %s\nComputed output: %s" % (
                                  out_expected, out_computed))
+
+    def test_and(self):
+        oper = AndOperation()
+        in_data_1 = [[2, 2], [3.3, 3], [5.7, 4]]
+        in_data_2 = [[2.5, 5], [4.7, 6]]
+        out_expected = [[2.5, 2], [3.3, 3], [5.7, 4]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 1st example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = [[2, 2], [3.3, 3], [4.7, 5]]
+        in_data_2 = [[2, 1], [3.3, 5], [4.7, 2]]
+        out_expected = [[2, 1], [3.3, 3], [4.7, 2]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 2nd example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = [[2, 2], [3.3, 3], [4.7, 5], [5, 5]]
+        in_data_2 = [[2, 1], [3.3, 5], [4.7, 3], [5, 5]]
+        out_expected = [[2, 1], [3.3, 3], [5, 5]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 3rd example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = [[2, 2], [3.3, 3]]
+        in_data_2 = [[4.7, 3], [5, 5]]
+        out_expected = [[4.7, 3]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 4th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = []
+        in_data_2 = [[4.7, 3], [5, 5]]
+        out_expected = []
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 5th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = [[1, 2], [4.1, 1], [5, 2], [6.1, 1], [6.7, 4], [9.9, 5]]
+        in_data_2 = [[1.2, 1], [3.7, 3], [7.5, 2], [8.1, 6]]
+        out_expected = [[1.2, 1], [3.7, 2], [4.1, 1], [5, 2], [6.1, 1], [6.7, 3], [7.5, 2], [8.1, 4], [9.9, 5]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 6th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
+        oper = AndOperation()
+        in_data_1 = [[1, 1], [2, 2], [3, 3]]
+        in_data_2 = [[3, 1], [4, 3], [7.5, 2]]
+        out_expected = [[3, 1], [4, 3], [7.5, 2]]
+        out_computed = oper.offline(in_data_1, in_data_2)
+
+        self.assertListEqual(out_expected, out_computed,
+                             "Problem with 7th example:\nExpected output: %s\nComputed output: %s" % (
+                                 out_expected, out_computed))
+
 
 if __name__ == '__main__':
     unittest.main()
