1cb92b9f9172734c83d0b8234d6ff6aa94464fe5
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2020-11-19 16:52:26 -0500

    filestructure change

--- tests/oper/test_stl_ct_offline.py
+++ tests/oper/test_stl_ct_offline.py
@@ -398,8 +398,13 @@ class TestSTLoffline(unittest.TestCase):
         oper = AndOperation()
         in_data_1 = [[2, 2], [3.3, 3], [5.7, 4]]
         in_data_2 = [[2.5, 5], [4.7, 6]]
+<<<<<<< HEAD
         out_expected = [[2.5, 2], [3.3, 3], [4.7, 3]]
         out_computed = oper.offline(in_data_1, in_data_2)
+=======
+        out_expected = [[2.5, 2], [3.3, 3], [5.7, 4]]
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
+>>>>>>> filestructure change
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 1st example:\nExpected output: %s\nComputed output: %s" % (
@@ -409,7 +414,7 @@ class TestSTLoffline(unittest.TestCase):
         in_data_1 = [[2, 2], [3.3, 3], [4.7, 5]]
         in_data_2 = [[2, 1], [3.3, 5], [4.7, 2]]
         out_expected = [[2, 1], [3.3, 3], [4.7, 2]]
-        out_computed = oper.offline(in_data_1, in_data_2)
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 2nd example:\nExpected output: %s\nComputed output: %s" % (
@@ -419,7 +424,7 @@ class TestSTLoffline(unittest.TestCase):
         in_data_1 = [[2, 2], [3.3, 3], [4.7, 5], [5, 5]]
         in_data_2 = [[2, 1], [3.3, 5], [4.7, 3], [5, 5]]
         out_expected = [[2, 1], [3.3, 3], [5, 5]]
-        out_computed = oper.offline(in_data_1, in_data_2)
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 3rd example:\nExpected output: %s\nComputed output: %s" % (
@@ -428,8 +433,13 @@ class TestSTLoffline(unittest.TestCase):
         oper = AndOperation()
         in_data_1 = [[2, 2], [3.3, 3]]
         in_data_2 = [[4.7, 3], [5, 5]]
+<<<<<<< HEAD
         out_expected = []
         out_computed = oper.offline(in_data_1, in_data_2)
+=======
+        out_expected = [[4.7, 3]]
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
+>>>>>>> filestructure change
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 4th example:\nExpected output: %s\nComputed output: %s" % (
@@ -439,7 +449,7 @@ class TestSTLoffline(unittest.TestCase):
         in_data_1 = []
         in_data_2 = [[4.7, 3], [5, 5]]
         out_expected = []
-        out_computed = oper.offline(in_data_1, in_data_2)
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 5th example:\nExpected output: %s\nComputed output: %s" % (
@@ -448,8 +458,13 @@ class TestSTLoffline(unittest.TestCase):
         oper = AndOperation()
         in_data_1 = [[1, 2], [4.1, 1], [5, 2], [6.1, 1], [6.7, 4], [9.9, 5]]
         in_data_2 = [[1.2, 1], [3.7, 3], [7.5, 2], [8.1, 6]]
+<<<<<<< HEAD
         out_expected = [[1.2, 1], [3.7, 2], [4.1, 1], [5, 2], [6.1, 1], [6.7, 3], [7.5, 2], [8.1, 4]]
         out_computed = oper.offline(in_data_1, in_data_2)
+=======
+        out_expected = [[1.2, 1], [3.7, 2], [4.1, 1], [5, 2], [6.1, 1], [6.7, 3], [7.5, 2], [8.1, 4], [9.9, 5]]
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
+>>>>>>> filestructure change
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 6th example:\nExpected output: %s\nComputed output: %s" % (
@@ -458,8 +473,13 @@ class TestSTLoffline(unittest.TestCase):
         oper = AndOperation()
         in_data_1 = [[1, 1], [2, 2], [3, 3]]
         in_data_2 = [[3, 1], [4, 3], [7.5, 2]]
+<<<<<<< HEAD
         out_expected = [[3, 1]]
         out_computed = oper.offline(in_data_1, in_data_2)
+=======
+        out_expected = [[3, 1], [4, 3], [7.5, 2]]
+        out_computed = oper.eval_wrapper(in_data_1, in_data_2)
+>>>>>>> filestructure change
 
         self.assertListEqual(out_expected, out_computed,
                              "Problem with 7th example:\nExpected output: %s\nComputed output: %s" % (
