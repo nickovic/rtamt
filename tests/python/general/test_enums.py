0e20dd5f38217559dc41cb4462f83aa4d31feb16
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2021-03-05 15:24:24 +0100

    Removed the error test.

--- tests/python/general/test_enums.py
+++ tests/python/general/test_enums.py
@@ -10,7 +10,6 @@ class TestEnums(unittest.TestCase):
         language = rtamt.Language.PYTHON
         time_interpretation = rtamt.TimeInterpretation.DISCRETE
         self.assertEqual('standard', str(semantics), 'Semantics')
-        self.assertEqual('standard-error', str(semantics), 'Semantics')
         self.assertEqual('python', str(language), 'Language')
         self.assertEqual('discrete_time', str(time_interpretation), 'TimeInterpretation')
 
