1e5c0b7fe3a4b2c522aba5db67a415eb56595f01
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-05-02 17:37:40 -0400

    update test

--- examples/offline_monitors/offline_stl.py
+++ examples/offline_monitors/offline_stl.py
@@ -5,4 +5,15 @@ sys.path.append(str(Path(__file__).parent.parent.parent))
 
 import rtamt
 
+time = [0, 2, 4, 7]
+a = [2, 5, 1, -7]
+
 spec = rtamt.STLrevDiscreteTimeSpecification()
+spec.name = 'STLab dense-time specification'
+spec.declare_var('a', 'float')
+spec.spec = 'always(a>=2)'
+spec.parse()
+
+rob = spec.evaluate(['time', time, 'a', a])
+
+print(rob)
