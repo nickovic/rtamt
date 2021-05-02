aa286173e5fe5daa0c7257bbe7f2d2f68f0290b3
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-05-02 15:53:04 -0400

    erase dense_time code

--- rtamt/semantics/abstract_semantics.py
+++ rtamt/semantics/abstract_semantics.py
@@ -8,7 +8,7 @@ from abc import ABCMeta, abstractmethod
 
 NOT_IMPLEMENTED = "You should implement this."
 
-class AbstractOperation:
+class AbstractSemantics:
     """
     Abstract Operation: template for any monitoring operation
     """
