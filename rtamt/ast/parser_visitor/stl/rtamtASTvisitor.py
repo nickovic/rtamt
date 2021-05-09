64fddebc2e6cb6d8e49dfc7c7d7ae164e995891b
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-05-09 17:01:52 -0400

    add visitNextLayerNodes method

--- rtamt/ast/parser_visitor/stl/rtamtASTvisitor.py
+++ rtamt/ast/parser_visitor/stl/rtamtASTvisitor.py
@@ -40,116 +40,106 @@ NOT_IMPLEMENTED = "You should implement this."
 class STLVisitor:
     __metaclass__ = ABCMeta
 
+    def visitNextLayerNodes(self, node, args):
+        for nodeChild in node.children:
+            self.visit(nodeChild, args)
+
     def visit(self, node, args):
         out = None
 
 
         if isinstance(node, Predicate):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitPredicate(node, args)
         elif isinstance(node, Variable):
             #Leasf does not need to vist anymore.
             out = self.visitVariable(node, args)
         elif isinstance(node, Neg):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitNot(node, args)
         elif isinstance(node, Disjunction):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitOr(node, args)
         elif isinstance(node, Conjunction):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitAnd(node, args)
         elif isinstance(node, Implies):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitImplies(node, args)
         elif isinstance(node, Iff):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitIff(node, args)
         elif isinstance(node, Xor):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitXor(node, args)
         elif isinstance(node, Eventually):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitEventually(node, args)
         elif isinstance(node, Always):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitAlways(node, args)
         elif isinstance(node, Until):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitUntil(node, args)
         elif isinstance(node, Once):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitOnce(node, args)
         elif isinstance(node, Historically):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitHistorically(node, args)
         elif isinstance(node, Since):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitSince(node, args)
         elif isinstance(node, TimedPrecedes):
             #Leasf does not need to vist anymore.
             out = self.visitTimedPrecedes(node, args)
         elif isinstance(node, Abs):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitAbs(node, args)
         elif isinstance(node, Addition):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitAddition(node, args)
         elif isinstance(node, Subtraction):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitSubtraction(node, args)
         elif isinstance(node, Multiplication):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitMultiplication(node, args)
         elif isinstance(node, Division):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitDivision(node, args)
         elif isinstance(node, Rise):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitRise(node, args)
         elif isinstance(node, Fall):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitFall(node, args)
         elif isinstance(node, Constant):
             #Leasf does not need to vist anymore.
             out = self.visitConstant(node, args)
         elif isinstance(node, Previous):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitPrevious(node, args)
         elif isinstance(node, Next):
-            out = self.visitNext(node, args)
+            self.visitNextLayerNodes(node, args)
             self.visit(node.children[0], args)
         elif isinstance(node, TimedUntil):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedUntil(node, args)
         elif isinstance(node, TimedAlways):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedAlways(node, args)
         elif isinstance(node, TimedEventually):
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedEventually(node, args)
-            self.visit(node.children[0], args)
         elif isinstance(node, TimedSince):
-            self.visit(node.children[0], args)
-            self.visit(node.children[1], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedSince(node, args)
         elif isinstance(node, TimedOnce):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedOnce(node, args)
         elif isinstance(node, TimedHistorically):
-            self.visit(node.children[0], args)
+            self.visitNextLayerNodes(node, args)
             out = self.visitTimedHistorically(node, args)
         else:
             #TODO: perhaps error?
