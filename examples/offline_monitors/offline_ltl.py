c022fafcbff8c4260a6bb547699113ee0386c80a
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-05-11 14:13:59 -0400

    testcase setting change

--- examples/offline_monitors/offline_ltl.py
+++ examples/offline_monitors/offline_ltl.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-from pathlib import Path
 import sys
-sys.path.append(str(Path(__file__).parent.parent.parent))
+sys.path.append('../../')
 
 import rtamt
 
 spec = rtamt.LTLrevSpecification()
+spec.name = 'STL test'
+spec.declare_var('a', 'float')
+spec.spec = 'always(a>=2)'
+spec.parse()
