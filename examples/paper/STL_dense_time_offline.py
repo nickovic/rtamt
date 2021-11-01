218344a4e2d13c00586ef44666c1a9b364bd11b2
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-11-01 16:03:44 -0400

    add STL_dense_time_offline.py

---
+++ examples/paper/STL_dense_time_offline.py
@@ -0,0 +1,12 @@
+import rtamt
+
+spec = rtamt.STLDenseTimeSpecification()
+spec.declare_var('req', 'float')
+spec.declare_var('gnt', 'float')
+spec.spec = '(req >= 3) implies (gnt >= 0)'
+spec.parse()
+
+req = [[0, 100], [1, -1], [2, -2], [3, 5], [4, -1]]
+gnt = [[0, 20], [1, -2], [2, 10], [3, 4], [4, -1]]
+
+rob = spec.update(['req', req], ['gnt', gnt])
