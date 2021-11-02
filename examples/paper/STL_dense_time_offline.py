611e4d8e05a695f38d14d12f9e0785b71a75d848
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2021-11-01 22:35:40 -0400

    small revision

--- examples/paper/STL_dense_time_offline.py
+++ examples/paper/STL_dense_time_offline.py
@@ -2,11 +2,11 @@ import rtamt
 
 spec = rtamt.STLDenseTimeSpecification()
 spec.declare_var('req', 'float')
-spec.declare_var('gnt', 'float')
-spec.spec = 'always((req >= 3) implies (eventually (gnt >= 0)))'
+spec.declare_var('event', 'float')
+spec.spec = 'always((req >= 3) implies (eventually[0, 2] (event >= 0)))'
 spec.parse()
 
-req = [[0, 100], [1, -1], [2, -2], [3, 5], [4, -1]]
-gnt = [[0, 20], [1, -2], [2, 10], [3, 4], [4, -1]]
+req   = [[0, 100], [1, -1], [2, -2], [3, 5], [4, -1]]
+event = [[0,  20], [1, -2], [2, 10], [3, 4], [4, -1]]
 
-rob = spec.evaluate(['req', req], ['gnt', gnt])
+rob = spec.evaluate(['req', req], ['event', event])
