6a76e821a6ba5f53d2457edb40216650d31b952c
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2022-01-07 15:08:57 +0100

    Updated properly the library call.

--- rtamt/spec/ltl/discrete_time/specification_parser.py
+++ rtamt/spec/ltl/discrete_time/specification_parser.py
@@ -53,8 +53,8 @@ class LTLSpecificationParser(LtlParserVisitor):
         io_type_name = 'rtamt.lib.rtamt_stl_library_wrapper.stl_io_type'
         comp_op_name = 'rtamt.lib.rtamt_stl_library_wrapper.stl_comp_op'
         if self.spec.language == Language.PYTHON:
-            io_type_name = 'rtamt.spec.stl.discrete_time.io_type'
-            comp_op_name = 'rtamt.spec.stl.discrete_time.comp_op'
+            io_type_name = 'rtamt.enumerations.io_type'
+            comp_op_name = 'rtamt.enumerations.comp_op'
 
         self.io_type_mod = __import__(io_type_name, fromlist=[''])
         self.comp_op_mod = __import__(comp_op_name, fromlist=[''])
