cda95af67d883593f537d78f08db9e26bedd8c8f
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-15 14:01:59 -0500

    skelton (WiP)

--- examples/multi_inheriting_skeleton.py
+++ examples/multi_inheriting_skeleton.py
@@ -3,19 +3,28 @@ from abc import ABCMeta
 from antlr4 import *
 #from antlr4.InputStream import InputStream
 
+DEBUG = True
 
-
-
+#---- closed ----#
 # dummys
 def InputStream(input):
     return None
 
 class XantrlLexer:
     def __init__(self, input):
+        # dummy
+        self.a = 100
+        print('init XantrlLexer')
         return None
 
 class XantrlParser:
-    def specification_file(input):
+    def __init__(self, input):
+        # dummy
+        print('int XantrlParser')
+        return None
+
+    def specification_file(self, input):
+        # dummy
         return None
 
 
@@ -32,6 +41,41 @@ class XANTRLparserVisitor(ParseTreeVisitor):
         return self.visitChildren(ctx)
 
 
+class AbstractAstPaser:
+
+    __metaclass__ = ABCMeta
+
+    def __init__(self, antrlLexer__class__, antrlParser__class__, astPaserVisitor__class__):
+        #TODO we need class check which inherits expected abstrauct class.
+        self.antrlLexer__class__ = antrlLexer__class__
+        self.antrlParser__class__ = antrlParser__class__
+        self.astPaserVisitor__class__ = astPaserVisitor__class__
+        return
+
+    def parse(self):
+        input_stream = InputStream(self.spec)
+        lexer = self.antrlLexer__class__(input_stream)
+        if DEBUG:
+            print(isinstance(lexer, XantrlLexer))
+            lexer = XantrlLexer(input_stream)
+            print(isinstance(lexer, XantrlLexer))
+
+        stream = CommonTokenStream(lexer)
+        parser = self.antrlParser__class__(stream)
+        #parser._listeners = [STLParserErrorListener()]
+        ctx = parser.specification_file()
+
+        # Create the visitor for the actual spec nodes
+        visitor = self.astPaserVisitor__class__()
+        self.node = visitor.visit(ctx.specification())
+
+        return self.node
+
+    def declare_var(self, name, type):
+        pass
+
+
+#---- user define ----#
 # Instance class of X language
 # /rtamt/rtamt/ast/parser/stl/discrete_time/specification_parser.py
 class XAstParserVisitor(XANTRLparserVisitor):
@@ -46,43 +90,15 @@ class XAstParserVisitor(XANTRLparserVisitor):
         return node
 
 
-class AbstractAstPaser:
-
-    __metaclass__ = ABCMeta
-
-    def __init__(self, antrlLexer, antrlParser, astPaserVisitor):
-        self.antrlLexer = antrlLexer
-        self.antrlParser = antrlParser
-        self.astPaserVisitor = astPaserVisitor
-        return
-
-    def parse(self):
-        input_stream = InputStream(self.spec)
-        lexer = self.antrlLexer(input_stream)
-        stream = CommonTokenStream(lexer)
-        parser = self.antrlParser(stream)
-        #parser._listeners = [STLParserErrorListener()]
-        ctx = parser.specification_file()
-
-        # Create the visitor for the actual spec nodes
-        visitor = self.astPaserVisitor()
-        self.node = visitor.visit(ctx.specification())
-
-        return self.node
-
-    def declare_var(self, name, type):
-        pass
-
-
 class XAstPaser(AbstractAstPaser):
 
     __metaclass__ = ABCMeta
 
     def __init__(self):
-        antrlLexer = XantrlLexer
-        antrlParser = XantrlParser
-        astPaserVisitor = XAstParserVisitor()
-        super(XAstPaser, self).__init__(antrlLexer, antrlParser, astPaserVisitor)
+        antrlLexer__class__ = XantrlLexer.__class__
+        antrlParser__class__ = XantrlParser.__class__
+        astPaserVisitor__class__ = XAstParserVisitor.__class__
+        super(XAstPaser, self).__init__(antrlLexer__class__, antrlParser__class__, astPaserVisitor__class__)
         return
 
 
@@ -92,4 +108,6 @@ xAstPaser = XAstPaser()
 xAstPaser.name = 'STL test'
 xAstPaser.declare_var('a', 'float') #Where should we put this? ast? or semanitcs?
 xAstPaser.spec = 'always(a>=2)'
-ast = xAstPaser.parse()
+node = xAstPaser.parse()
+
+#NamePrintAstVistor(node)
