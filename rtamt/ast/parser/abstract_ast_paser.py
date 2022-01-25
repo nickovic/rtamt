7527431cd73bdf792b94fc571651f6be0f33e8a5
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-25 17:54:07 -0500

    go through declare_var() issue AbstractAstPaser in (WiP)

--- rtamt/ast/parser/abstract_ast_paser.py
+++ rtamt/ast/parser/abstract_ast_paser.py
@@ -1,4 +1,4 @@
-from abc import ABCMeta
+from abc import ABCMeta, abstractmethod
 
 from antlr4 import *
 from antlr4.InputStream import InputStream
@@ -10,13 +10,25 @@ class AbstractAstPaser:
     __metaclass__ = ABCMeta
 
     def __init__(self, antrlLexerType, antrlParserType, astPaserVisitorType):
-        self.ops = set()
-        #self.spec = spec
 
+        # Class of lexser, parser, paserVisitor
         #TODO we need class check which inherits expected abstrauct class.
         self.antrlLexerType = antrlLexerType
         self.antrlParserType = antrlParserType
         self.astPaserVisitorType = astPaserVisitorType
+
+        # Attributes
+        self.ops = set()    #TODO Do we need it?
+        #self.spec = spec
+
+        self.vars = set()
+        self.free_vars = set()
+        self.var_type_dict = dict()
+        self.var_object_dict = dict()
+        self.var_io_dict = dict()
+
+        self.var_topic_dict = dict()
+
 
         #TODO Tom did not understand it well. 
         # 1) Is it only stl? I don't think so
@@ -69,7 +81,77 @@ class AbstractAstPaser:
     def ops(self, ops):
         self.__ops = ops
 
-    # TODO move from spec.
-    # rtamt/rtamt/spec/ltl/discrete_time/specification.py
-    #def declare_var(self, var_name, var_type):
-    #    pass
+    @property
+    def vars(self):
+        return self.__vars
+
+    @vars.setter
+    def vars(self, vars):
+        self.__vars = vars
+
+    @property
+    def free_vars(self):
+        return self.__free_vars
+
+    @free_vars.setter
+    def free_vars(self, free_vars):
+        self.__free_vars = free_vars
+
+    def add_var(self, var):
+        self.vars.add(var)
+
+    def create_var_from_name(self, var_name):
+        var = None
+        var_type = self.var_type_dict[var_name]
+        if var_type.encode('utf-8') == 'float'.encode('utf-8'):
+            var = float()
+        elif var_type.encode('utf-8') == 'int'.encode('utf-8'):
+            var = int()
+        elif var_type.encode('utf-8') == 'complex'.encode('utf-8'):
+            var = complex()
+        else:
+            try:
+                var_module = self.modules[var_type]
+                class_ = getattr(var_module, var_type)
+                var = class_()
+            except KeyError:
+                raise STLParseException('The type {} does not seem to be imported.'.format(var_type))
+        return var
+
+    def create_var_from_name(self, var_name):
+        var = None
+        var_type = self.var_type_dict[var_name]
+        if var_type.encode('utf-8') == 'float'.encode('utf-8'):
+            var = float()
+        elif var_type.encode('utf-8') == 'int'.encode('utf-8'):
+            var = int()
+        elif var_type.encode('utf-8') == 'complex'.encode('utf-8'):
+            var = complex()
+        else:
+            try:
+                var_module = self.modules[var_type]
+                class_ = getattr(var_module, var_type)
+                var = class_()
+            except KeyError:
+                raise STLParseException('The type {} does not seem to be imported.'.format(var_type))
+        return var
+
+    def declare_var(self, var_name, var_type):
+        if var_name in self.vars:
+            logging.warning(
+                'Variable {} was already declared. It is now overriden with the new declaration.'.format(var_name))
+
+        # Associate to variable name 'var' its type 'type'
+        self.var_type_dict[var_name] = var_type
+
+        # Add variable name 'var' to the set of variables
+        self.add_var(var_name)
+        self.free_vars.add(var_name)
+        instance = self.create_var_from_name(var_name)
+        self.var_object_dict[var_name] = instance
+
+        # Add the default variable topic to var
+        #TODO perhaps, we may omit it from AST.
+        self.var_topic_dict[var_name] = 'rtamt/{}'.format(var_name)
+
+        self.var_io_dict[var_name] = 'output'
