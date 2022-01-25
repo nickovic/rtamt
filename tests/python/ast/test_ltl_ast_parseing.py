763ff9e47c6f1aaf1401406d937de5ad49fd7b78
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-25 12:09:09 -0500

    test case for LtlAstPaser

---
+++ tests/python/ast/test_ltl_ast_parseing.py
@@ -0,0 +1,17 @@
+import unittest
+
+from rtamt.ast.parser.ltl.specification_parser import LtlAstParser
+
+class TestLtlAstParsing(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestLtlAstParsing, self).__init__(*args, **kwargs)
+
+    def test_parse(self):
+        astPaser = LtlAstParser()
+        astPaser.declare_var('a', 'float')
+        astPaser.spec = 'always(a>=2)'
+        ast = astPaser.parse()
+
+if __name__ == '__main__':
+    unittest.main()
