814affc7272bef6536f291239efd7aa5833d562f
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-01-26 20:44:59 -0500

    fix test setting miss.

--- tests/python/ast/test_ltl_ast_parseing.py
+++ tests/python/ast/test_ltl_ast_parseing.py
@@ -8,11 +8,10 @@ class TestLtlAstParsing(unittest.TestCase):
         super(TestLtlAstParsing, self).__init__(*args, **kwargs)
 
     def test_parse(self):
-        with self.assertRaises(RuntimeError):
-            astPaser = LtlAstParser()
-            astPaser.declare_var('a', 'float')
-            astPaser.spec = 'always(a>=2)'
-            ast = astPaser.parse()
+        astPaser = LtlAstParser()
+        astPaser.declare_var('a', 'float')
+        astPaser.spec = 'always(a>=2)'
+        ast = astPaser.parse()
 
 if __name__ == '__main__':
     unittest.main()
