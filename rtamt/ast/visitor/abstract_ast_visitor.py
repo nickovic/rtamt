a635601a69884fbd9181f5bba46091325bb5f587
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-02-02 00:07:11 -0500

    add visitSpecific

--- rtamt/ast/visitor/abstract_ast_visitor.py
+++ rtamt/ast/visitor/abstract_ast_visitor.py
@@ -12,29 +12,29 @@ class AbstractAstVisitor(object):
 
     def visitAbstractAstChildren(self, node, *args, **kwargs):
         if isinstance(node, UnaryNode):
-            sample_from_child = self.abstractAstVisit(node.children[0])
-            #sample_return = node.interrupt(sample_from_child, *args, **kwargs)
+            sample_from_child = self.visitAbstractAstChildren(node.children[0])
+            sample_return = self.visitSpecific(sample_from_child, *args, **kwargs)
             print(node.__class__.__name__)
             sample_return = None
             return sample_return
         elif isinstance(node, BinaryNode):
-            sample_from_child_left = self.abstractAstVisit(node.children[0])
-            sample_from_child_right = self.abstractAstVisit(node.children[1])
-            #sample_return = node.interrupt(sample_from_child_left, sample_from_child_right, *args, **kwargs)
+            sample_from_child_left = self.visitAbstractAstChildren(node.children[0])
+            sample_from_child_right = self.visitAbstractAstChildren(node.children[1])
+            sample_return = self.visitSpecific(sample_from_child_left, sample_from_child_right, *args, **kwargs)
             print(node.__class__.__name__)
             sample_return = None
             return sample_return
         if isinstance(node, LeafNode):
-            #sample_return = node.interrupt(*args, **kwargs)
+            sample_return = self.visitSpecific(*args, **kwargs)
             print(node.__class__.__name__)
             sample_return = None
             return sample_return
         else:
             raise AstVisitorException('{} is not ANTRL4 AST node'.format(node.__class__.__name__))
 
-    def abstractAstVisit(self, node, *args, **kwargs):
-        sample_return = self.visitAbstractAstChildren(node, *args, **kwargs)
-        return sample_return
-
     def visit(self, ast, *args, **kwargs):
-        return self.abstractAstVisit(ast.ast, *args, **kwargs)
+        return self.visitAbstractAstChildren(ast.ast, *args, **kwargs)
+
+    def visitSpecific(self, *args, **kwargs):
+        sample_return = None
+        return sample_return
