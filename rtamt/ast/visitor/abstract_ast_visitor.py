e51cfd700325187cf8a4f05a0bf90ef679502ed8
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-02-03 22:56:41 -0500

    return miss

--- rtamt/ast/visitor/abstract_ast_visitor.py
+++ rtamt/ast/visitor/abstract_ast_visitor.py
@@ -16,17 +16,14 @@ class AbstractAstVisitor(object):
         if isinstance(node, UnaryNode):
             sample_from_child = self.visitAbstractAstChildren(node.children[0])
             sample_return = self.visitSpecific(node, sample_from_child, *args, **kwargs)
-            sample_return = None
             return sample_return
         elif isinstance(node, BinaryNode):
             sample_from_child_left = self.visitAbstractAstChildren(node.children[0])
             sample_from_child_right = self.visitAbstractAstChildren(node.children[1])
             sample_return = self.visitSpecific(node, sample_from_child_left, sample_from_child_right, *args, **kwargs)
-            sample_return = None
             return sample_return
         if isinstance(node, LeafNode):
             sample_return = self.visitSpecific(node, *args, **kwargs)
-            sample_return = None
             return sample_return
         else:
             raise AstVisitorException('{} is not RTAMT AST node'.format(node.__class__.__name__))
