02435a5a98ac084f26f9f66b23725e6778eed916
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-02-09 16:31:06 -0500

    add abstract name visitor and test

---
+++ tests/python/ast/test_ast_visitor.py
@@ -0,0 +1,41 @@
+import unittest
+
+from rtamt.ast.parser.ltl.specification_parser import LtlAst
+from rtamt.ast.parser.stl.discrete_time.specification_parser import stlDiscreteTimeAst
+from rtamt.ast.parser.stl.dense_time.specification_parser import stlDenseTimeAst
+
+from tests.python.ast.print_name_ast_visitor import PrintNameAstVisitor
+
+
+class TestPrintVisitor(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestPrintVisitor, self).__init__(*args, **kwargs)
+        self.visitor = PrintNameAstVisitor()
+
+    def test_visitor_with_ltl(self):
+        ast = LtlAst()
+        ast.declare_var('a', 'float')
+        ast.spec = 'always(a>=2)'
+        ast.parse()
+        out = self.visitor.visitAst(ast)
+        self.assertEqual(out, '((Variable)Predicate(Constant))', 'ltl assertion')
+
+    def test_visitor_with_discrete_time_stl(self):
+        ast = stlDiscreteTimeAst()
+        ast.declare_var('a', 'float')
+        ast.spec = 'always(a>=2)'
+        ast.parse()
+        out = self.visitor.visitAst(ast)
+        self.assertEqual(out, '((Variable)Predicate(Constant))', 'stl assertion')
+
+    def test_visitor_with_dence_time_stl(self):
+        ast = stlDenseTimeAst()
+        ast.declare_var('a', 'float')
+        ast.spec = 'always(a>=2)'
+        ast.parse()
+        out = self.visitor.visitAst(ast)
+        self.assertEqual(out, '((Variable)Predicate(Constant))', 'stl assertion')
+
+
+if __name__ == '__main__':
+    unittest.main()
