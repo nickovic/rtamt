afcbbe7816946d297e1656b91a62e4d1a685a744
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-02-09 09:19:44 -0500

    abstract visitor

--- rtamt/ast/visitor/abstract_visitor.py
+++ rtamt/ast/visitor/abstract_visitor.py
@@ -1,14 +1,39 @@
 from abc import ABCMeta
 
+from rtamt.node.binary_node import BinaryNode
+from rtamt.node.unary_node import UnaryNode
+from rtamt.node.leaf_node import LeafNode
+
+from rtamt.exception.exception import AstVisitorException
+
+
 class AbstractVisitor(object):
     __metaclass__ = ABCMeta
 
     def visitChildren(self, node, *args, **kwargs):
-        children_output = list()
+        results = list()
         for nodeChild in node.children:
-            output = self.visit(nodeChild, *args, **kwargs)
-            children_output.append(output)
-        return children_output
+            result = self.visit(nodeChild, *args, **kwargs)
+            results.append(result)
+        return results
 
     def visit(self, node, *args, **kwargs):
+        if isinstance(node, BinaryNode):
+            result = self.visitBinary(node, *args, **kwargs)
+        elif isinstance(node, UnaryNode):
+            result = self.visitUnary(node, *args, **kwargs)
+        elif isinstance(node, LeafNode):
+            result = self.visitLeaf(node, *args, **kwargs)
+        else:
+            raise AstVisitorException('{} is not RTAMT AST node'.format(node.__class__.__name__))
+        return result
+
+
+    def visitBinary(self, node, *args, **kwargs):
+        return self.visitChildren(node, *args, **kwargs)
+
+    def visitUnary(self, node, *args, **kwargs):
+        return self.visitChildren(node, *args, **kwargs)
+
+    def visitLeaf(self, node, *args, **kwargs):
         return self.visitChildren(node, *args, **kwargs)
