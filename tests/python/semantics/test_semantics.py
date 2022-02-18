87a21c6d344b9fed1e8aa98e7ec0d0de410e273e
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-02-18 08:57:58 -0500

    test case miss

--- tests/python/semantics/test_semantics.py
+++ tests/python/semantics/test_semantics.py
@@ -1,11 +1,13 @@
 import unittest
 
-from rtamt.ast.parser.ltl.specification_parser import LtlAst
 from rtamt.ast.parser.stl.discrete_time.specification_parser import stlDiscreteTimeAst
 from rtamt.ast.parser.stl.dense_time.specification_parser import stlDenseTimeAst
 
 from rtamt.operation.abstract_discrete_time_offline_ast_visitor import discrete_time_offline_ast_visitor_factory
+from rtamt.operation.abstract_dense_time_offline_ast_visitor import dense_time_offline_ast_visitor_factory
+
 from rtamt.operation.stl.discrete_time.offline.ast_visitor import StlDiscreteTimeOfflineAstVisitor
+from rtamt.operation.stl.dense_time.offline.ast_visitor import StlDenseTimeOfflineAstVisitor
 
 
 class TestSemantics(unittest.TestCase):
@@ -13,14 +15,16 @@ class TestSemantics(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestSemantics, self).__init__(*args, **kwargs)
 
-        self.ast = stlDiscreteTimeAst()
-        self.ast.declare_var('a', 'float')
-        self.ast.spec = 'always(a>=2)'
-        self.ast.parse()
+
 
     def test_stl_discrete_time_parse(self):
+        ast = stlDiscreteTimeAst()  #TODO use same stlAst
+        ast.declare_var('a', 'float')
+        ast.spec = 'always(a>=2)'
+        ast.parse()
+
         stlDiscreteTimeOfflineAstVisitor = discrete_time_offline_ast_visitor_factory(StlDiscreteTimeOfflineAstVisitor)()
-        stlDiscreteTimeOfflineAstVisitor.ast = self.ast
+        stlDiscreteTimeOfflineAstVisitor.ast = ast
         dataset = {
             'time': [0, 1, 2, 3, 4],
             'a': [100, -1, -2, 5, -1],
@@ -28,8 +32,17 @@ class TestSemantics(unittest.TestCase):
         rob = stlDiscreteTimeOfflineAstVisitor.evaluate(dataset)
         print(rob)
 
+    def test_stl_dense_time_parse(self):
+        ast = stlDenseTimeAst()  #TODO use same stlAst
+        ast.declare_var('a', 'float')
+        ast.spec = 'always(a>=2)'
+        ast.parse()
 
-
+        stlDenseTimeOfflineAstVisitor = dense_time_offline_ast_visitor_factory(StlDenseTimeOfflineAstVisitor)()
+        stlDenseTimeOfflineAstVisitor.ast = ast
+        a = [[0, 1.3], [0.7, 3], [1.3, 0.1], [2.1, -2.2]]
+        rob = stlDenseTimeOfflineAstVisitor.evaluate([['a', a]])
+        print(rob)
 
 if __name__ == '__main__':
     unittest.main()
