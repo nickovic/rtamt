e942d668ddd64978447878e1622cce6c9bb426bd
Tomoya Yamaguchi <tomoya.yamaguchi@toyota.com>
2022-03-09 09:22:04 -0500

    add DenseTimeOnlineUpdateFinalVisitor

--- rtamt/operation/abstract_dense_time_online_evaluator.py
+++ rtamt/operation/abstract_dense_time_online_evaluator.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import operator
 
+from rtamt.node.ltl.variable import Variable
+from rtamt.node.ltl.constant import Constant
+
 from rtamt.ast.visitor.abstract_ast_visitor import AbstractAstVisitor
 from rtamt.operation.abstract_online_evaluator import AbstractOnlineEvaluator, AbstractOnlineUpdateVisitor
 from rtamt.operation.dense_time_evaluator import DenseTimeEvaluator
@@ -13,6 +16,7 @@ class AbstractDenseTimeOnlineEvaluator(AbstractOnlineEvaluator, DenseTimeEvaluat
         AbstractOnlineEvaluator.__init__(self)
         DenseTimeEvaluator.__init__(self)
         self.updateVisitor = DenseTimeOnlineUpdateVisitor()
+        self.updateFinalVisitor = DenseTimeOnlineUpdateFinalVisitor()
         return
 
     #input format
@@ -30,11 +34,23 @@ class AbstractDenseTimeOnlineEvaluator(AbstractOnlineEvaluator, DenseTimeEvaluat
         # evaluate spec forest
         rob = self.updateVisitor.visitAst(self.ast, self.online_operator_dict, self.ast.var_object_dict)[0]
 
-        self.ast.var_object_dict = self.ast.var_object_dict.fromkeys(self.ast.var_object_dict, [])  #TODO I did not understant it.
+        self.ast.var_object_dict = self.ast.var_object_dict.fromkeys(self.ast.var_object_dict, [])  #TODO I did not understand it.
 
         return rob
 
+    def update_final(self, dataset):
+        # check ast exists
+        self.exist_ast()
 
+        # update the value of every input variable
+        self.set_variable_to_ast_from_dataset(dataset)
+
+        # evaluate spec forest
+        rob = self.updateFinalVisitor.visitAst(self.ast, self.online_operator_dict, self.ast.var_object_dict)[0]
+
+        self.ast.var_object_dict = self.ast.var_object_dict.fromkeys(self.ast.var_object_dict, [])  #TODO I did not understand it.
+
+        return rob
 
 
 class DenseTimeOnlineUpdateVisitor(AbstractOnlineUpdateVisitor):
@@ -49,6 +65,46 @@ class DenseTimeOnlineUpdateVisitor(AbstractOnlineUpdateVisitor):
     def visitConstant(self, node, online_operator_dict, var_object_dict):
         sample_return = [[0, node.val], [float("inf"), node.val]]
         return sample_return
+
+
+class DenseTimeOnlineUpdateFinalVisitor(AbstractAstVisitor):
+    def visitSpec(self, node, online_operator_dict, var_object_dict):
+        sample_return = self.visit(node, online_operator_dict, var_object_dict)
+        var_object_dict[node] = sample_return  #TODO subspec name is necessary as a key for var_object_dict.
+        return sample_return
+
+    def visitBinary(self, node, online_operator_dict, var_object_dict):
+        sample_left  = self.visit(node.children[0], online_operator_dict, var_object_dict)
+        sample_right = self.visit(node.children[1], online_operator_dict, var_object_dict)
+        operator = online_operator_dict[node.name]
+        sample_return = operator.update_final(sample_left, sample_right)
+        return sample_return
+
+    def visitUnary(self, node, online_operator_dict, var_object_dict):
+        sample = self.visit(node.children[0], online_operator_dict, var_object_dict)
+        operator = online_operator_dict[node.name]
+        sample_return = operator.update_final(sample)
+        return sample_return
+
+    def visitLeaf(self, node, online_operator_dict, var_object_dict):
+        if isinstance(node, Constant):
+            sample_return = self.visitConstant(node, online_operator_dict, var_object_dict)
+        elif isinstance(node, Variable):
+            sample_return = self.visitVariable(node, online_operator_dict, var_object_dict)
+        return sample_return
+
+    def visitVariable(self, node, online_operator_dict, var_object_dict):
+        var = var_object_dict[node.var]
+        if node.field:  #TODO Tom did not understand this line.
+            sample_return = operator.attrgetter(node.field)(var)
+        else:
+            sample_return = var
+        return sample_return
+
+    def visitConstant(self, node, online_operator_dict, var_object_dict):
+        sample_return = [[0, node.val], [float("inf"), node.val]]
+        return sample_return
+
 
 def dense_time_online_evaluator_factory(AstVisitor):
     if not issubclass(AstVisitor, AbstractAstVisitor):  # type check
