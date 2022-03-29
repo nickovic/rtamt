4ef306858a771677b1681ff2b1ae16495350e82c
Tomoya Yamaguchi <3110yamaguchi@mtj.biglobe.ne.jp>
2022-03-29 14:38:59 -0400

    small revisions

--- examples/journal/STL_dense_time_offline.py
+++ examples/journal/STL_dense_time_offline.py
@@ -3,7 +3,7 @@ import rtamt
 spec = rtamt.StlDenseTimeOfflineSpecification()
 spec.declare_var('req', 'float')
 spec.declare_var('gnt', 'float')
-spec.spec = 'G((req >=3)->(F[0,5](gnt>= 3)))'
+spec.spec = 'G((req>=3)->(F[0,5](gnt>=3)))'
 spec.parse()
 
 req = [[0.0, 0.0], [2.0, 6.0], [4.0, 0.0], [10.0, 0.0]]
