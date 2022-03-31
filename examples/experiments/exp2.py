9f0c3e013cea78492c9110f1ae1c5ce433ee0670
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2022-03-31 08:46:16 -0400

    exp2

--- examples/experiments/exp2.py
+++ examples/experiments/exp2.py
@@ -3,220 +3,176 @@ import sys
 import random
 import time
 import matplotlib.pyplot as plt
+from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeOnlineSpecification
+from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeOnlineSpecificationCpp
 
-t1 = list(range(10))
-t2 = list(range(100))
-t3 = list(range(1000))
-t4 = list(range(10000))
-t5 = list(range(100000))
-t6 = list(range(1000000))
+t = list(range(10000))
 
-v1 = [random.random() for i in range(10)]
-v2 = [random.random() for i in range(100)]
-v3 = [random.random() for i in range(1000)]
-v4 = [random.random() for i in range(10000)]
-v5 = [random.random() for i in range(100000)]
-v6 = [random.random() for i in range(1000000)]
+v = [random.random() for i in range(10000)]
+w = [random.random() for i in range(10000)]
 
-w1 = [random.random() for i in range(10)]
-w2 = [random.random() for i in range(100)]
-w3 = [random.random() for i in range(1000)]
-w4 = [random.random() for i in range(10000)]
-w5 = [random.random() for i in range(100000)]
-w6 = [random.random() for i in range(1000000)]
-
-m1 = [[i, random.random()] for i in range(10)]
-m2 = [[i, random.random()] for i in range(100)]
-m3 = [[i, random.random()] for i in range(1000)]
-m4 = [[i, random.random()] for i in range(10000)]
-m5 = [[i, random.random()] for i in range(100000)]
-m6 = [[i, random.random()] for i in range(1000000)]
-
-n1 = [[i, random.random()] for i in range(10)]
-n2 = [[i, random.random()] for i in range(100)]
-n3 = [[i, random.random()] for i in range(1000)]
-n4 = [[i, random.random()] for i in range(10000)]
-n5 = [[i, random.random()] for i in range(100000)]
-n6 = [[i, random.random()] for i in range(1000000)]
 
 s1_times = []
 s2_times = []
-s3_times = []
-s4_times = []
 
 
-spec1 = rtamt.STLDiscreteTimeSpecification(semantics=rtamt.Semantics.STANDARD)
+spec1 = StlDiscreteTimeOnlineSpecification()
 spec1.name = 'Example 1'
 spec1.declare_var('req', 'float')
 spec1.declare_var('gnt', 'float')
 spec1.declare_var('out', 'float')
 spec1.set_var_io_type('req', 'input')
 spec1.set_var_io_type('gnt', 'output')
-spec1.spec = 'out = (req>=3)'
+spec1.spec = 'out = ((req>=3) implies (once[0:10](gnt>=3)))'
 try:
     spec1.parse()
 except rtamt.STLParseException as err:
     print('STL Parse Exception: {}'.format(err))
     sys.exit()
 
-spec2 = rtamt.STLDiscreteTimeSpecification(semantics=rtamt.Semantics.STANDARD)
+spec2 = StlDiscreteTimeOnlineSpecification()
 spec2.name = 'Example 1'
 spec2.declare_var('req', 'float')
 spec2.declare_var('gnt', 'float')
 spec2.declare_var('out', 'float')
 spec2.set_var_io_type('req', 'input')
 spec2.set_var_io_type('gnt', 'output')
-spec2.spec = 'out = ((req>=3) implies (gnt>=3))'
+spec2.spec = 'out = ((req>=3) implies (once[0:100](gnt>=3)))'
 try:
     spec2.parse()
 except rtamt.STLParseException as err:
     print('STL Parse Exception: {}'.format(err))
     sys.exit()
 
-spec3 = rtamt.STLDiscreteTimeSpecification(semantics=rtamt.Semantics.STANDARD)
+spec3 = StlDiscreteTimeOnlineSpecification()
 spec3.name = 'Example 1'
 spec3.declare_var('req', 'float')
 spec3.declare_var('gnt', 'float')
 spec3.declare_var('out', 'float')
 spec3.set_var_io_type('req', 'input')
 spec3.set_var_io_type('gnt', 'output')
-spec3.spec = 'out = ((req>=3) implies (eventually[0:5](gnt>=3)))'
+spec3.spec = 'out = ((req>=3) implies (once[0:1000](gnt>=3)))'
 try:
     spec3.parse()
 except rtamt.STLParseException as err:
     print('STL Parse Exception: {}'.format(err))
     sys.exit()
 
-spec4 = rtamt.STLDiscreteTimeSpecification(semantics=rtamt.Semantics.STANDARD)
+spec4 = StlDiscreteTimeOnlineSpecification()
 spec4.name = 'Example 1'
 spec4.declare_var('req', 'float')
 spec4.declare_var('gnt', 'float')
 spec4.declare_var('out', 'float')
 spec4.set_var_io_type('req', 'input')
 spec4.set_var_io_type('gnt', 'output')
-spec4.spec = 'out = ((H(req>=3)) implies ((not (req>=3))until[0:5](gnt>=3)))'
+spec4.spec = 'out = ((req>=3) implies (once[0:10000](gnt>=3)))'
 try:
     spec4.parse()
 except rtamt.STLParseException as err:
     print('STL Parse Exception: {}'.format(err))
     sys.exit()
 
-dataset = {'time': t1, 'req': v1, 'gnt': w1}
 s_t = time.time()
-spec1.evaluate(dataset)
+[spec1.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s1_times.append(e_t-s_t)
-s_t = time.time()
-spec2.evaluate(dataset)
-e_t = time.time()
-s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-dataset = {'time': t2, 'req': v2, 'gnt': w2}
 s_t = time.time()
-spec1.evaluate(dataset)
+[spec2.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s1_times.append(e_t-s_t)
-s_t = time.time()
-spec2.evaluate(dataset)
-e_t = time.time()
-s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-dataset = {'time': t3, 'req': v3, 'gnt': w3}
 s_t = time.time()
-spec1.evaluate(dataset)
+[spec3.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s1_times.append(e_t-s_t)
-s_t = time.time()
-spec2.evaluate(dataset)
-e_t = time.time()
-s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-dataset = {'time': t4, 'req': v4, 'gnt': w4}
 s_t = time.time()
-spec1.evaluate(dataset)
+[spec4.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s1_times.append(e_t-s_t)
+
+spec1 = StlDiscreteTimeOnlineSpecificationCpp()
+spec1.name = 'Example 1'
+spec1.declare_var('req', 'float')
+spec1.declare_var('gnt', 'float')
+spec1.declare_var('out', 'float')
+spec1.set_var_io_type('req', 'input')
+spec1.set_var_io_type('gnt', 'output')
+spec1.spec = 'out = ((req>=3) implies (once[0:10](gnt>=3)))'
+try:
+    spec1.parse()
+except rtamt.STLParseException as err:
+    print('STL Parse Exception: {}'.format(err))
+    sys.exit()
+
+spec2 = StlDiscreteTimeOnlineSpecificationCpp()
+spec2.name = 'Example 1'
+spec2.declare_var('req', 'float')
+spec2.declare_var('gnt', 'float')
+spec2.declare_var('out', 'float')
+spec2.set_var_io_type('req', 'input')
+spec2.set_var_io_type('gnt', 'output')
+spec2.spec = 'out = ((req>=3) implies (once[0:100](gnt>=3)))'
+try:
+    spec2.parse()
+except rtamt.STLParseException as err:
+    print('STL Parse Exception: {}'.format(err))
+    sys.exit()
+
+spec3 = StlDiscreteTimeOnlineSpecificationCpp()
+spec3.name = 'Example 1'
+spec3.declare_var('req', 'float')
+spec3.declare_var('gnt', 'float')
+spec3.declare_var('out', 'float')
+spec3.set_var_io_type('req', 'input')
+spec3.set_var_io_type('gnt', 'output')
+spec3.spec = 'out = ((req>=3) implies (once[0:1000](gnt>=3)))'
+try:
+    spec3.parse()
+except rtamt.STLParseException as err:
+    print('STL Parse Exception: {}'.format(err))
+    sys.exit()
+
+spec4 = StlDiscreteTimeOnlineSpecificationCpp()
+spec4.name = 'Example 1'
+spec4.declare_var('req', 'float')
+spec4.declare_var('gnt', 'float')
+spec4.declare_var('out', 'float')
+spec4.set_var_io_type('req', 'input')
+spec4.set_var_io_type('gnt', 'output')
+spec4.spec = 'out = ((req>=3) implies (once[0:10000](gnt>=3)))'
+try:
+    spec4.parse()
+except rtamt.STLParseException as err:
+    print('STL Parse Exception: {}'.format(err))
+    sys.exit()
+
 s_t = time.time()
-spec2.evaluate(dataset)
+[spec1.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-dataset = {'time': t5, 'req': v5, 'gnt': w5}
 s_t = time.time()
-spec1.evaluate(dataset)
-e_t = time.time()
-s1_times.append(e_t-s_t)
-s_t = time.time()
-spec2.evaluate(dataset)
+[spec2.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-dataset = {'time': t6, 'req': v6, 'gnt': w6}
 s_t = time.time()
-spec1.evaluate(dataset)
-e_t = time.time()
-s1_times.append(e_t-s_t)
-s_t = time.time()
-spec2.evaluate(dataset)
+[spec3.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
 e_t = time.time()
 s2_times.append(e_t-s_t)
-s_t = time.time()
-spec3.evaluate(dataset)
-e_t = time.time()
-s3_times.append(e_t-s_t)
-s_t = time.time()
-spec4.evaluate(dataset)
-e_t = time.time()
-s4_times.append(e_t-s_t)
 
-x = [10, 100, 1000, 10000, 100000, 1000000]
+s_t = time.time()
+[spec4.update(i, [['req', v[i]], ['gnt', w[i]]]) for i in t]
+e_t = time.time()
+s2_times.append(e_t-s_t)
+
+x = [10, 100, 1000, 10000]
 fig, ax = plt.subplots()
 line1 = ax.plot(x, s1_times)
 line2 = ax.plot(x, s2_times)
-line3 = ax.plot(x, s3_times)
-line4 = ax.plot(x, s4_times)
-plt.title('Offline monitors - Computation time vs. |Formula| and |Input|')
-plt.xlabel('# of samples per signal')
+plt.title('Online monitors - Python vs. C++')
+plt.xlabel('Upper bound in the temporal modality')
 plt.ylabel('Time (s)')
-plt.legend([line1, line2, line3, line4], [r'\varphi_1', r'\varphi_2', r'\varphi_3', r'\varphi_4'])
 plt.show()
