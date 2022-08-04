84e910eb24b3db89e2eeea8528cc84b04634e428
Tomoya Yamaguchi <3110yamaguchi@mtj.biglobe.ne.jp>
2022-08-04 15:44:29 -0400

    edit __init__.py for naming issue.

--- rtamt/__init__.py
+++ rtamt/__init__.py
@@ -14,11 +14,11 @@ from rtamt.semantics.enumerations.io_type import StlIOType
 from rtamt.semantics.enumerations.options import Language, Semantics, TimeInterpretation
 
 from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeSpecification
-from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeSpecification as StlSpecification
+from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeSpecification as STLSpecification # for old API
 from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeOfflineSpecification
 from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeOnlineSpecification
 from rtamt.spec.stl.discrete_time.specification import StlDiscreteTimeOnlineSpecificationCpp
 from rtamt.spec.stl.dense_time.specification import StlDenseTimeSpecification
-from rtamt.spec.stl.dense_time.specification import StlDenseTimeSpecification as STLCTSpecification
+from rtamt.spec.stl.dense_time.specification import StlDenseTimeSpecification as STLCTSpecification # for old API
 from rtamt.spec.stl.dense_time.specification import StlDenseTimeOfflineSpecification
 from rtamt.spec.stl.dense_time.specification import StlDenseTimeOnlineSpecification
