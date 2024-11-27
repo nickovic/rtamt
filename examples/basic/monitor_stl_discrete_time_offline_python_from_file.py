16fe0da7b6e041cfa3472dab3c87d25a9693fa84
Dejan Nickovic <dejan.nickovic@ait.ac.at>
2024-11-27 10:04:54 +0000

    Added an example of loading an STL spec from a file.

--- examples/basic/monitor_stl_discrete_time_offline_python_from_file.py
+++ examples/basic/monitor_stl_discrete_time_offline_python_from_file.py
@@ -5,12 +5,11 @@ def monitor():
     # data
     dataSet = {
          'time': [0, 1, 2],
-         'a': [100.0, -1.0, -2.0],
-         'b': [20.0, 2.0, -10.0]
+         'velocity': [12.0, 11.0, 9.3],
     }
 
     # # stl
-    spec = rtamt.StlDiscreteTimeSpecification()
+    spec = rtamt.StlDiscreteTimeOfflineSpecification()
     spec.name = 'STL discrete-time online Python monitor'
     spec.spec = spec.get_spec_from_file('./spec.stl')
 
