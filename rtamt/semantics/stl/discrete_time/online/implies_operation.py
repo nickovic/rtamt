747698a8ccc9f48f06f712198218f9d5320cbe4d
Thomas Ederer <thomas.ederer@hochschule-burgenland.at>
2026-06-24 15:09:45 +0200

    Changed the IMPLIES operator to use sys.float_info.min for the left argument when it is zero. This prevents implications that are always true, even when they should not be.

--- rtamt/semantics/stl/discrete_time/online/implies_operation.py
+++ rtamt/semantics/stl/discrete_time/online/implies_operation.py
@@ -1,4 +1,5 @@
 from rtamt.semantics.abstract_online_operation import AbstractOnlineOperation
+from sys import float_info
 
 class ImpliesOperation(AbstractOnlineOperation):
     def __init__(self):
@@ -8,5 +9,6 @@ class ImpliesOperation(AbstractOnlineOperation):
         pass
 
     def update(self, sample_left, sample_right):
+        if sample_left == 0: sample_left = float_info.min
         sample_return = max(-sample_left, sample_right)
         return sample_return
