59f296cc0edb7d88614b456a9cb4270b1d91ed26
Thomas Ederer <thomas.ederer@hochschule-burgenland.at>
2026-06-25 13:36:03 +0200

    Set version of antlr4 to 4.9 to get the typing fixes. Deleted StlLexer files because they are outdated and no grammar file (.g4) exists to build them with 4.9.

--- rtamt/antlr/parser/ltl/LtlLexer.py
+++ rtamt/antlr/parser/ltl/LtlLexer.py
@@ -1,4 +1,4 @@
-# Generated from LtlLexer.g4 by ANTLR 4.7.2
+# Generated from LtlLexer.g4 by ANTLR 4.9
 from antlr4 import *
 from io import StringIO
 from typing import TextIO
@@ -496,7 +496,7 @@ class LtlLexer(Lexer):
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.7.2")
+        self.checkVersion("4.9")
         self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
         self._actions = None
         self._predicates = None
--- rtamt/antlr/parser/ltl/LtlParser.py
+++ rtamt/antlr/parser/ltl/LtlParser.py
@@ -1,4 +1,4 @@
-# Generated from LtlParser.g4 by ANTLR 4.7.2
+# Generated from LtlParser.g4 by ANTLR 4.9
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -270,7 +270,7 @@ class LtlParser ( Parser ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.7.2")
+        self.checkVersion("4.9")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
